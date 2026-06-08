6a6217784142957e32fa2e865f3fad1b03424ce1
TherPelz <tpelz2003@gmail.com>
2026-06-08 21:30:19 +0200

    Dateien zum Testen erweitert

---
+++ Smarte Mülltonne/Software/Test_2/Schrittmotor_PWM.py
@@ -0,0 +1,70 @@
+from machine import Pin, PWM
+from time import sleep, sleep_us
+
+
+class StepperPWM:
+    def __init__(self, dir_pin, step_pin, enable_pin, step_freq=1000, name="STEPPER", debug=True):
+        self.dir = Pin(dir_pin, Pin.OUT)
+        self.step_pin = Pin(step_pin, Pin.OUT)
+        self.enable = Pin(enable_pin, Pin.OUT)
+
+        self.pwm = PWM(self.step_pin)
+        self.pwm.duty_u16(0)
+
+        self.step_freq = step_freq
+        self.name = name
+        self.debug = debug
+
+        self.disable()
+
+    def enable_motor(self):
+        self.enable.value(0)   # A4988: LOW = ON
+
+    def disable(self):
+        self.pwm.duty_u16(0)
+        self.enable.value(1)   # A4988: HIGH = OFF
+
+    def move(self, steps, direction):
+        self.enable_motor()
+        self.dir.value(direction)
+
+        sleep_us(20)  # kurze Zeit, damit DIR stabil ist
+
+        if self.debug:
+            print(self.name, "dir:", direction, "steps:", steps, "freq:", self.step_freq)
+
+        self.pwm.freq(self.step_freq)
+        self.pwm.duty_u16(32768)  # 50 Prozent Duty Cycle
+
+        move_time = steps / self.step_freq
+        sleep(move_time)
+
+        self.disable()
+
+
+# ---------------- TEST ----------------
+
+motor1 = StepperPWM(
+    dir_pin=10,
+    step_pin=11,
+    enable_pin=12,
+    step_freq=1000,
+    name="Motor 1"
+)
+
+motor2 = StepperPWM(
+    dir_pin=13,
+    step_pin=14,
+    enable_pin=15,
+    step_freq=1000,
+    name="Motor 2"
+)
+
+while True:
+    print("Motor 2 forward")
+    motor2.move(16550, 1)
+    sleep(1)
+
+    print("Motor 2 backward")
+    motor2.move(16550, 0)
+    sleep(1)
---
+++ Smarte Mülltonne/Software/Test_2/Schrittmotoren_gemeinsam.py
@@ -0,0 +1,96 @@
+from machine import Pin
+from time import sleep, sleep_us
+
+
+class DualStepper:
+    def __init__(self,left_dir_pin,left_step_pin,left_enable_pin,right_dir_pin,right_step_pin,right_enable_pin,delay_us=2,enable_active_value=1):
+        self.left_dir = Pin(left_dir_pin, Pin.OUT)
+        self.left_step = Pin(left_step_pin, Pin.OUT)
+        self.left_enable = Pin(left_enable_pin, Pin.OUT)
+
+        self.right_dir = Pin(right_dir_pin, Pin.OUT)
+        self.right_step = Pin(right_step_pin, Pin.OUT)
+        self.right_enable = Pin(right_enable_pin, Pin.OUT)
+
+        self.delay_us = delay_us
+        self.enable_active_value = enable_active_value
+        self.disable_value = 0 if enable_active_value == 1 else 1
+
+        self.left_step.value(0)
+        self.right_step.value(0)
+        self.disable()
+
+    def enable(self):
+        self.left_enable.value(self.enable_active_value)
+        self.right_enable.value(self.enable_active_value)
+
+    def disable(self):
+        self.left_enable.value(self.disable_value)
+        self.right_enable.value(self.disable_value)
+
+    def move_together(self, steps, left_dir, right_dir):
+        self.enable()
+
+        self.left_dir.value(left_dir)
+        self.right_dir.value(right_dir)
+
+        for _ in range(steps):
+            self.left_step.value(1)
+            self.right_step.value(1)
+            sleep_us(self.delay_us)
+
+            self.left_step.value(0)
+            self.right_step.value(0)
+            sleep_us(self.delay_us)
+
+        self.disable()
+
+    def geradeaus_fahrt(self, steps):
+        self.move_together(steps, left_dir=1, right_dir=1)
+
+    def rueckwaerts_fahrt(self, steps):
+        self.move_together(steps, left_dir=0, right_dir=0)
+
+    def drehung_90_links(self):
+        self.move_together(STEPS_90_GRAD, left_dir=0, right_dir=1)
+
+    def drehung_90_rechts(self):
+        self.move_together(STEPS_90_GRAD, left_dir=1, right_dir=0)
+
+    def drehung_180_links(self):
+        self.move_together(STEPS_180_GRAD, left_dir=0, right_dir=1)
+
+    def drehung_180_rechts(self):
+        self.move_together(STEPS_180_GRAD, left_dir=1, right_dir=0)
+
+
+# -------------------------------------------------
+# KALIBRIERUNG
+# -------------------------------------------------
+
+STEPS_GERADEAUS_TEST = 5000
+STEPS_90_GRAD = 16550
+STEPS_180_GRAD = STEPS_90_GRAD * 2
+
+# Falls Enable falsch herum ist:
+# enable_active_value=1 testen.
+# Wenn Motoren dauerhaft aus bleiben, auf 0 ändern.
+
+motors = DualStepper(left_dir_pin=10,left_step_pin=11,left_enable_pin=12,right_dir_pin=13,right_step_pin=14,right_enable_pin=15,delay_us=2,enable_active_value=1)
+
+while True:
+    print("Geradeaus")
+    motors.geradeaus_fahrt(STEPS_GERADEAUS_TEST)
+    sleep(2)
+
+    print("90 Grad links")
+    motors.drehung_90_links()
+    sleep(2)
+
+    print("90 Grad rechts")
+    motors.drehung_90_rechts()
+    sleep(2)
+
+    print("180 Grad links")
+    motors.drehung_180_links()
+    sleep(2)
