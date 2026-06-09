82d184200d0565e86dee37792e346343d1737f30
TherPelz <tpelz2003@gmail.com>
2026-06-09 13:46:18 +0200

    Update Multiplexer_Test_Liniensensor.py
    Überarbeitungen

--- Smarte Mülltonne/Software/Test_2/Multiplexer_Test_Liniensensor.py
+++ Smarte Mülltonne/Software/Test_2/Multiplexer_Test_Liniensensor.py
@@ -9,6 +9,9 @@ s3 = machine.Pin(5, machine.Pin.OUT)
 # SIG/Z vom Multiplexer an GP28
 mux_signal = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
+# Bit-Zahl bilden
+# &1    -> nur erstes Bit von rechts betrachten
+# >>1   -> Binärzahl um 1 nach rechts verschieben
 def select_channel(channel):
     s0.value(channel & 1)
     s1.value((channel >> 1) & 1)
@@ -30,24 +33,3 @@ while True:
         print("Hell / keine Linie")
 
     utime.sleep(0.3)
-
-
-# import machine
-# import utime
-
-# s0 = machine.Pin(2, machine.Pin.OUT)
-# s1 = machine.Pin(3, machine.Pin.OUT)
-# s2 = machine.Pin(4, machine.Pin.OUT)
-# s3 = machine.Pin(5, machine.Pin.OUT)
-
-# mux_signal = machine.Pin(28, machine.Pin.IN)
-
-# # C0
-# s0.value(0)
-# s1.value(0)
-# s2.value(0)
-# s3.value(0)
-
-# while True:
-#     print("C0:", mux_signal.value())
-#     utime.sleep(0.2)
